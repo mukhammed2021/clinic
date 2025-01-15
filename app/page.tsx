3c4c202e3eb48ed2ee3d234e15c3ecc8ea447286
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-15 12:07:25 +0500

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
 }
