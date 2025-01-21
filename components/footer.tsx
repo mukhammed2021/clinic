c66d4c6e182b2714e1456a2c6f21230b2b3f6223
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-21 12:33:37 +0500

    update style

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <div className="container flex flex-col items-center justify-between gap-4 pb-6 text-xs max-[953px]:text-center min-[953px]:flex-row min-[1082px]:text-base">
+      <div className="container flex flex-col items-center justify-between gap-4 pb-6 pt-[1.875rem] text-xs max-[953px]:text-center min-[953px]:flex-row min-[1082px]:text-base">
         <div>
           <p>Согласие на обработку персональных данных</p>
           <p>
